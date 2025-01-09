9c68121def2cebfb84dd96e5c8692d8482a261ca
Héctor J. Vásquez <me@vasen.dev>
2025-01-09 10:09:18 -0600

    feat: update harpoon config

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,3 +1,43 @@
+-- basic telescope configuration
+local function toggle_telescope(harpoon_files)
+  local conf = require("telescope.config").values
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  local finder = function()
+    local paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(paths, item.value)
+    end
+
+    return require("telescope.finders").new_table({
+      results = paths,
+    })
+  end
+
+  require("telescope.pickers")
+    .new({}, {
+      prompt_title = "Harpoon",
+      finder = finder(),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+      attach_mappings = function(prompt_bufnr, map)
+        map({ "n", "i" }, "<C-d>", function()
+          local state = require("telescope.actions.state")
+          local selected_entry = state.get_selected_entry()
+          local current_picker = state.get_current_picker(prompt_bufnr)
+
+          table.remove(harpoon_files.items, selected_entry.index)
+          current_picker:refresh(finder())
+        end)
+        return true
+      end,
+    })
+    :find()
+end
+
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
@@ -12,7 +52,34 @@ return {
           },
         },
       },
-      lazy = true,
     },
   },
+  keys = function()
+    local keys = {
+      {
+        "<leader>ee",
+        function()
+          toggle_telescope(require("harpoon"):list())
+        end,
+      },
+      {
+        "<leader>ea",
+        function()
+          require("harpoon"):list():add()
+        end,
+      },
+    }
+
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Harpoon #" .. i,
+      })
+    end
+
+    return keys
+  end,
 }
