c8189018c815dd93bfbea019bb5cb1b4707c4ba4
Héctor J. Vásquez <me@vasen.dev>
2025-01-23 10:42:48 -0600

    feat: add smart check for obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,17 +1,30 @@
+local vault_path = "/Users/vasen/Library/Mobile Documents/iCloud~md~obsidian/Documents/Main Vault"
+
+local function exists(name)
+  local ok, _, code = os.rename(name, name)
+
+  if not ok and code ~= 13 then
+    return false
+  end
+
+  return true
+end
+
 return {
   "epwalsh/obsidian.nvim",
   version = "*",
   lazy = false,
+  enabled = exists(vault_path),
   ft = "markdown",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   opts = {
-    ui = { enable = false },
+    ui = { enable = true },
     workspaces = {
       {
         name = "Main",
-        path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Main Vault",
+        path = vault_path,
       },
     },
   },
