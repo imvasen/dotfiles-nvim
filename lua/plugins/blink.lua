b61db9678f2d9067e2c2dfa2b5dee756d0a5030c
Héctor J. Vásquez <me@vasen.dev>
2025-04-10 18:25:05 -0600

    feat: disable ghost text from blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,15 +1,19 @@
 return {
   "Saghen/blink.cmp",
   opts = {
-    keymap = {
-      preset = "default",
-    },
     completion = {
       menu = { auto_show = true },
+      ghost_text = { enabled = false },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 0,
       },
+    },
+    keymap = {
+      preset = "default",
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
     },
   },
 }
