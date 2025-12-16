322a5d750cfc551851f3195e3b58feeb20e33a26
Héctor J. Vásquez <me@vasen.dev>
2025-12-16 15:13:08 -0600

    feat: add ignored files to view

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,13 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        always_show_by_pattern = { ".env*" },
+        never_show = { "node_modules", ".git" },
+      },
+    },
     window = {
       position = "right",
     },
